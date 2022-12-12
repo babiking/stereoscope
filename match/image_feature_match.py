8f5b18307c9e110188007ddb3bbc81094159b04e
babiking <brojackfeely@163.com>
2022-12-12 22:05:28 +0800

    add orb feature match

---
+++ match/image_feature_match.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+
+def match_keypoints_2d(src_img, dst_img, threshold=0.75, draw=False):
+    src_gray = cv.cvtColor(src_img, cv.COLOR_BGR2GRAY)
+    dst_gray = cv.cvtColor(dst_img, cv.COLOR_BGR2GRAY)
+
+    # 1. feature extractor / detector
+    detector = cv.ORB_create()
+
+    src_kp, src_desc = detector.detectAndCompute(src_gray, None)
+    dst_kp, dst_desc = detector.detectAndCompute(dst_gray, None)
+
+    # 2. feature descriptor match
+    FLANN_INDEX_LSH = 6
+    index_params = dict(algorithm=FLANN_INDEX_LSH,
+                        table_number=6,
+                        key_size=12,
+                        multi_probe_level=1)
+    search_params = dict(checks=50)
+    matcher = cv.FlannBasedMatcher(index_params, search_params)
+    matches = matcher.knnMatch(src_desc, dst_desc, k=2)
+
+    # 3. filter good matches
+    good = []
+    for m, n in matches:
+        if m.distance < threshold * n.distance:
+            good.append([m])
+
+    if draw:
+        cat_img = cv.drawMatchesKnn(
+            src_img,
+            src_kp,
+            dst_img,
+            dst_kp,
+            good,
+            None,
+            flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+        plt.imshow(cat_img[:, :, ::-1])
+        plt.show()
+    return src_kp, src_desc, dst_kp, dst_desc, good
+
+
+def main():
+    src_rgb = cv.imread('data/bandsaw1/im0.png', flags=cv.IMREAD_COLOR)
+    dst_rgb = cv.imread('data/bandsaw1/im1.png', flags=cv.IMREAD_COLOR)
+
+    match_keypoints_2d(src_rgb, dst_rgb, 0.75, True)
+
+
+if __name__ == '__main__':
+    main()
